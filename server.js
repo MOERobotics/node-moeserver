646a609828dc36d7ab92bf76eaa82c7b0be12a40
Kevok93 <kevok93@gmail.com>
2016-07-04 12:23:43 -0400

    Reformatting

--- server.js
+++ server.js
@@ -1,59 +1,54 @@
-var server=require('node-http-server');
-var ws = require("nodejs-websocket");
+var server     = require('node-http-server');
+var websock    = require("nodejs-websocket");
 var v4l2camera = require("v4l2camera");
+
 var response
-var cam = new v4l2camera.Camera("/dev/video0");
+var cam = new v4l2camera.Camera("/dev/video0"); //Should we try to find it? What if it's on /dev/video1? What if we have multiple cameras?
 
 // camera memes
 
+if (cam.configGet().formatName !== "MJPG") {
+    console.log("NOTICE: MJPG camera required");
+    process.exit(1);
+}
+cam.start();
 
-    if (cam.configGet().formatName !== "MJPG") {
-      console.log("NOTICE: MJPG camera required");
-      process.exit(1);
-    }
-    cam.start();
+// end camera memes
 
-    // end camera memes
-
-
-    server.deploy(
-        {
-            port:80,
-            root:'~/imports'
-        }
-    );
+server.deploy({
+    port : 80,
+    root : '~/imports'
+});
 
 
-    var server = ws.createServer(function (conn) {
-    	console.log("New connection")
-    	conn.on("text", function (str) {
-    		console.log("Received "+str)
+var server = ws.createServer( function (conn) {
+	console.log("New connection")
+	conn.on("text", function (str) {
+		console.log("Received "+str)
         response = JSON.parse(str)
-// gonna replace this with a switch but im lazy
-
-        if (response.gripcam == true) {
-          // settings values for grip mode
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-
-        }else if(response.gripcam == false){
-          // settings for not
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-          cam.controlSet(id, value)
-        }else {
-          console.log("idk wut u say");
+        switch (response.gripcam) {
+            case true:  // settings values for mars mode
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                break
+            case false: // settings values for earth mode
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                cam.controlSet(id, value)
+                break;
+            default: 
+                console.log("idk wut u say");
+                break;
         }
-
-    	})
-    	conn.on("close", function (code, reason) {
-    		console.log("Connection closed")
-    	})
-    }).listen(8001)
+    }); //</ontext>
+	conn.on("close", function (code, reason) {
+		console.log("Connection closed")
+	})
+}).listen(8001)
