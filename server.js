c1586ac200fa890925faea81ce5b7df52a4b82a2
kevok <kevok@galacticentertainment.net>
2016-08-24 20:01:44 -0400

    I don't even

--- server.js
+++ server.js
@@ -3,45 +3,49 @@ var websock    = require("nodejs-websocket");
 var v4l2camera = require("v4l2camera");
 
 var response
-var cam = new v4l2camera.Camera("/dev/video0"); //Should we try to find it? What if it's on /dev/video1? What if we have multiple cameras?
+//var cam = new v4l2camera.Camera("/dev/video0"); //Should we try to find it? What if it's on /dev/video1? What if we have multiple cameras?
 
 // camera memes
 
-if (cam.configGet().formatName !== "MJPG") {
+//if (cam.configGet().formatName !== "MJPG") {
     console.log("NOTICE: MJPG camera required");
-    process.exit(1);
-}
-cam.start();
+//    process.exit(1);
+//}
+//cam.start();
 
 // end camera memes
 
 server.deploy({
-    port : 80,
-    root : '~/imports'
+    port : 8081,
+    root : 'imports/'
 });
 
 
-var server = ws.createServer( function (conn) {
+var server = websock.createServer( function (conn) {
 	console.log("New connection")
 	conn.on("text", function (str) {
 		console.log("Received "+str)
         response = JSON.parse(str)
         switch (response.gripcam) {
             case true:  // settings values for mars mode
+            /*
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
+            */
                 break
             case false: // settings values for earth mode
+            /*
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
                 cam.controlSet(id, value)
+            */
                 break;
             default: 
                 console.log("idk wut u say");
